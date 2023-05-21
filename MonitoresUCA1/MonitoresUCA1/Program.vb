0b1dd3b18e76dec9891fb1901974045f863d4f15
Ed Centeno <cchavarriacenteno8@gmail.com>
2023-05-21 02:20:21 -0600

    Formulario padre creado

---
+++ MonitoresUCA1/MonitoresUCA1/Program.vb
@@ -0,0 +1,9 @@
+﻿Imports MonitoresUCA1.My
+
+Public Class Program
+    Public Shared Sub Main(args As String())
+        Dim ap As New MyApplication()
+        ap.Run(args)
+
+    End Sub
+End Class
