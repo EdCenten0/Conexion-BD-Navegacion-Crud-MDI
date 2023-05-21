afc7f3f95bdce680c4406943308c52847215e2b8
Ed Centeno <cchavarriacenteno8@gmail.com>
2023-05-21 00:48:55 -0600

    Botones de posicion con sus eventos programados

--- MonitoresUCA1/MonitoresUCA1/Form1.vb
+++ MonitoresUCA1/MonitoresUCA1/Form1.vb
@@ -8,4 +8,36 @@
         Me.CiudadTableAdapter.Fill(Me.DsCiudades.Ciudad)
 
     End Sub
+
+    Public Sub MostrarPosicion()
+        Dim iTotal As Integer = CiudadBindingSource.Count
+        Dim iPos As Integer
+        If iTotal = 0 Then
+            etPosicion.Text = "No. de Registros"
+        Else
+            iPos = CiudadBindingSource.Position + 1
+            etPosicion.Text = iPos.ToString & " de " & iTotal.ToString
+        End If
+
+    End Sub
+
+    Private Sub btPrimero_Click(sender As Object, e As EventArgs) Handles btPrimero.Click
+        CiudadBindingSource.Position = 0
+        MostrarPosicion()
+    End Sub
+
+    Private Sub btAnterior_Click(sender As Object, e As EventArgs) Handles btAnterior.Click
+        CiudadBindingSource.Position -= 1
+        MostrarPosicion()
+    End Sub
+
+    Private Sub btSiguiente_Click(sender As Object, e As EventArgs) Handles btSiguiente.Click
+        CiudadBindingSource.Position += 1
+        MostrarPosicion()
+    End Sub
+
+    Private Sub btUltimo_Click(sender As Object, e As EventArgs) Handles btUltimo.Click
+        CiudadBindingSource.Position = CiudadBindingSource.Count - 1
+        MostrarPosicion()
+    End Sub
 End Class
